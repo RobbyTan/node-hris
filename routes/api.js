3340053b17d85e48613f43850da5a8596d03b99e
Irvin Delano <irvindelano@gmail.com>
2018-05-08 15:17:07 +0700

    fixed bug in viewAttendances

--- routes/api.js
+++ routes/api.js
@@ -10,11 +10,9 @@ router.use(methodOverride("_method"));
 router.get('/attendancesfull', async(req, res) => {
   let from = req.query.from
   let to = req.query.to
-  console.log('true')
 
   if (from && to) {
-    console.log('here')
-    res.json(await showData(from, to, true))
+    res.json(await getFullAttendancesJSON(from, to))
   } else {
     res.json({})
   }
@@ -26,7 +24,7 @@ router.get('/attendances', async(req, res) => {
   console.log('false ')
 
   if (from && to) {
-    res.json(await showData(from, to, false))
+    res.json(await getAttendancesJSON(from, to))
   } else {
     res.json({})
   }
@@ -119,50 +117,67 @@ router.post("/attendances", (req, res) => {
   });
 })
 
-// String,String,Boolean
-async function showData (from, to, full) {
-  let fromString = from;
-  let toString = to;
+// String, String
+async function getAttendancesJSON (fromString, toString) {
+  let databaseResult = await loadResultFromDatabase(fromString, toString);
+  return databaseResult;
+}
 
-  return await full ? loadResultFromDatabaseFull(fromString) : loadResultFromDatabase(fromString, toString);
+async function getFullAttendancesJSON (fromString, toString) {
+  let days = Math.abs(moment(fromString, 'YYYY-MM-DD').diff(moment(toString, 'YYYY-MM-DD'), 'days')) + 1
+  let fromDate = moment(fromString, 'YYYY-MM-DD')
+  let dataPromise = []
+  for (let i = 0; i < days; i++) {
+    let currentString = fromDate.year() + '-' + (fromDate.month() + 1) + '-' + fromDate.date()
+    dataPromise.push(loadResultFromDatabaseFull(currentString))
+    fromDate.add(1, 'days')
+  }
+  let results = await Promise.all(dataPromise)
+  return {data: concatArray(results), date: fromString}
+}
+
+function concatArray (arr) {
+  if (arr.length === 1) { return arr[0] } else {
+    return arr.reduce((acc, next) => acc.concat(next), [])
+  }
 }
 
 function loadResultFromDatabaseFull (date) {
   return db.Employee.aggregate([
-  {
-    $lookup:
     {
-      'from': 'attendances',
-      'localField': 'nik',
-      'foreignField': 'nik',
-      'as': 'data_absensi'
-    }
-  },
-  {
-    $project: {
-      startDate: 1,
-      nik: 1,
-      first_Name: 1,
-      last_Name: 1,
-      birthday: 1,
-      department: 1,
-      jam_masuk: 1,
-      atasan_langsung: 1,
-      absensi: {
-        $filter: {
-          input: '$data_absensi',
-          as: 'absensi',
-          cond: {$and: [{$gte: ['$$absensi.date', new Date(date)]}, {$lt: ['$$absensi.date', moment(date, 'YYYY-MM-DD').add(1, 'days').toDate()]}]}
+      $lookup:
+      {
+        'from': 'attendances',
+        'localField': 'nik',
+        'foreignField': 'nik',
+        'as': 'data_absensi'
+      }
+    },
+    {
+      $project: {
+        startDate: 1,
+        nik: 1,
+        first_Name: 1,
+        last_Name: 1,
+        birthday: 1,
+        department: 1,
+        jam_masuk: 1,
+        atasan_langsung: 1,
+        absensi: {
+          $filter: {
+            input: '$data_absensi',
+            as: 'absensi',
+            cond: {$and: [{$gte: ['$$absensi.date', new Date(date)]}, {$lt: ['$$absensi.date', moment(date, 'YYYY-MM-DD').add(1, 'days').toDate()]}]}
+          }
         }
       }
+    },
+    {
+      $match: {
+        'department': {$nin: ['Resign', null]}
+      }
     }
-  },
-  {
-    $match: {
-      'department': {$nin: ['Resign', null]}
-    }
-  }
-  ])
+  ]);
 }
 
 function loadResultFromDatabase (fromDateStr, toDateStr) {
