3b747f6c6ff78305ae60d6c5b9dfc706969999d5
Irvin Delano <irvindelano@gmail.com>
2018-05-15 12:18:22 +0700

    payroll Dosen Tidak Tetap logic

--- public/scripts/payroll/payroll-dosen-calculator.js
+++ public/scripts/payroll/payroll-dosen-calculator.js
@@ -1,12 +1,21 @@
-let PayrollCalculator = (function() {
+let PayrollDosenCalculator = (function() {
   let fullDataDictionary = {};
+
+  function _toMiliSeconds(time) {
+    let timeParts = time.split(":");
+    let h = timeParts[0] ? +timeParts[0] * 60 * 60 * 1000 : 0;
+    let m = timeParts[1] ? +timeParts[1] * 60 * 1000 : 0;
+    let s = timeParts[2] ? +timeParts[2] * 1000 : 0;
+    return h + m + s;
+  }
 
   function _initFullDataDictionary() {
     $.getJSON("/api/pph", rows => {
       for (let row of rows) {
         fullDataDictionary[row.nik] = {
           kategori_pegawai: row.kategori_pegawai,
-  
+          nik: row.nik,
+
           //nama lengkap sesuai KTP
           nama_depan: row.nama_depan,
           nama_tengah: row.nama_tengah,
@@ -31,7 +40,8 @@ let PayrollCalculator = (function() {
   }
 
   function process(attendances, options) {
-    let totalDurasiKerja = {};
+    attendances = attendances.filter(attendance => attendance.kategori_pegawai==='Dosen Tidak Tetap');
+    let totalDurasiKerjaMs = {};
     for (let attendance of attendances) {
       let clocks = attendance.absensi.map(clock => moment(clock.date));
       let clockPairingResult = ClockPairing.process(clocks, {
@@ -39,19 +49,25 @@ let PayrollCalculator = (function() {
         department: attendance.department
       });
       // akumulasi durasi kerja dosen
-      if (!totalDurasiKerja[attendance.nik]) totalDurasiKerja[attendance.nik] = 0;
-      totalDurasiKerja[attendance.nik] += clockPairingResult.totalWorkingTime;
+      if (!totalDurasiKerjaMs[attendance.nik]) totalDurasiKerjaMs[attendance.nik] = 0;
+      totalDurasiKerjaMs[attendance.nik] += _toMiliSeconds(clockPairingResult.totalWorkingTime);
     }
-
     let payrollResult = [];
-    for (let nik in totalDurasiKerja) {
+    for (let nik in totalDurasiKerjaMs) {
+      if (!fullDataDictionary[nik]) continue;
+      let rateGajiPerJamStr = fullDataDictionary[nik].jumlah_gaji_saat_ini;
+      let rateGajiPerJam = +(rateGajiPerJamStr.replace(/[,\. ]/g, ''));
+      let gajiPokok = (totalDurasiKerjaMs[nik]/(60*60*1000.0))*rateGajiPerJam;
+      let totalGaji = gajiPokok;
       let payrollObject = {
         ...fullDataDictionary[nik],
-        total_durasi_kerja: totalDurasiKerja[nik],
-        total_gaji: totalDurasiKerja[nik]*fullDataDictionary[nik].gaji_pokok
+        total_durasi_kerja_ms: totalDurasiKerjaMs[nik],
+        gaji_pokok: gajiPokok,
+        total_gaji: totalGaji
       };
       payrollResult.push(payrollObject);
     }
+    console.log('payrollResult', payrollResult);
     return payrollResult;
   }
 
